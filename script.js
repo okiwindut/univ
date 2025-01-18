701bd6b71f061d55de4f57bcac7dc2d424c2b76f
okiwindut <91321679+okiwindut@users.noreply.github.com>
2025-01-18 18:43:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// const menuLinks = document.querySelectorAll('.space-y-2 a');
+// const contentDiv = document.getElementById('content');
+
+// menuLinks.forEach(link => {
+//     link.addEventListener('click', (event) => {
+//         event.preventDefault();
+
+//         const menu = event.target.textContent.trim();
+
+//         switch (menu) {
+//             case "HOME":
+//                 contentDiv.innerHTML = "<p>Ini adalah konten untuk halaman HOME.</p>";
+//                 break;
+//             case "MAGANG":
+//                 contentDiv.innerHTML = "<p>Ini adalah konten untuk halaman MAGANG. Informasi terkait magang akan ditampilkan di sini.</p>";
+//                 break;
+//             default:
+//                 contentDiv.innerHTML = "<p>Menu tidak ditemukan.</p>";
+//         }
+//     });
+// });
